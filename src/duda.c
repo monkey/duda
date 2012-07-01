80f28b4a527ec80e36269c86004c784d25d76521
Eduardo Silva <edsiper@gmail.com>
2012-07-01 13:52:27 -0600

    Events: fix worker list of events
    Signed-off-by: Eduardo Silva <edsiper@gmail.com>

--- src/duda.c
+++ src/duda.c
@@ -270,6 +270,7 @@ void _mkp_core_thctx()
 {
     struct mk_list *head_vs, *head_ws, *head_gl;
     struct mk_list *list_events_write;
+    struct mk_list *events_list;
     struct vhost_services *entry_vs;
     struct web_service *entry_ws;
     duda_global_t *entry_gl;
@@ -281,6 +282,10 @@ void _mkp_core_thctx()
     list_events_write = mk_api->mem_alloc(sizeof(struct mk_list));
     mk_list_init(list_events_write);
     pthread_setspecific(duda_global_events_write, (void *) list_events_write);
+
+    events_list = mk_api->mem_alloc(sizeof(struct mk_list));
+    mk_list_init(events_list);
+    pthread_setspecific(duda_events_list, (void *) events_list);
 
     /*
      * Load global data if applies, this is toooo recursive, we need to go through
