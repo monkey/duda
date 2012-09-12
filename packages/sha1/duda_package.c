0ec4bfa02b26cdea3580861d223aca6ebcc72e02
Eduardo Silva <edsiper@gmail.com>
2012-09-11 19:41:24 -0600

    SHA1: Document package and method
    Signed-off-by: Eduardo Silva <edsiper@gmail.com>

--- packages/sha1/duda_package.c
+++ packages/sha1/duda_package.c
@@ -19,18 +19,37 @@
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
+/*
+ * @OBJ_NAME: sha1
+ * @OBJ_DESC: The SHA1 package provides the cryptographic hash function to encode
+ * any data with the SHA1 algorithm.
+ * @PKG_HEADER: #include "packages/sha1/sha1.h"
+ * @PKG_INIT: duda_load_package(sha1, "sha1");
+ */
+
 #include <stdlib.h>
 
 #include "duda_package.h"
 #include "sha1.h"
 
-static void sha1_encode (const void *dataIn, unsigned char *dataOut,
+/*
+ * @METHOD_NAME: encode
+ * @METHOD_DESC: It encodes an input data stream with SHA1 algorithm and writes
+ * the output to data_out buffer. Finally it stores the encoded data length in
+ * the length variable.
+ * @METHOD_PROTO: void encode(const void *data_in, unsigned char *data_out, unsigned long length)
+ * @METHOD_PARAM: data_in the source data to be encoded
+ * @METHOD_PARAM: data_out the buffer where the encoded data is written
+ * @METHOD_PARAM: length the length of data_out
+ * @METHOD_RETURN: None
+ */
+static void sha1_encode (const void *data_in, unsigned char *data_out,
                          unsigned long length)
 {
     SHA_CTX sha;
     SHA1_Init(&sha);
-    SHA1_Update(&sha, dataIn, length);
-    SHA1_Final(dataOut, &sha);
+    SHA1_Update(&sha, data_in, length);
+    SHA1_Final(data_out, &sha);
 }
 
 struct duda_api_sha1 *get_sha1_api()
