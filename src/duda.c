1768d404f2966e4425666be41af182fc1d3d68e1
Eduardo Silva <eduardo@monkey.io>
2014-08-04 23:11:53 -0600

    Core: Initialize pthread_key duda_logger_fmt_cache
    This pthread key was not initialized, generating a corruption on
    Jemalloc when exiting the threads.
    
    Signed-off-by: Eduardo Silva <eduardo@monkey.io>
    
    Conflicts:
    	src/duda.c

--- src/duda.c
+++ src/duda.c
@@ -586,6 +586,7 @@ int _mkp_init(struct plugin_api **api, char *confdir)
     pthread_key_create(&duda_events_list, NULL);
     pthread_key_create(&duda_global_events_write, NULL);
     pthread_key_create(&duda_global_dr_list, NULL);
+    pthread_key_create(&duda_logger_fmt_cache, NULL);
 
     mk_list_init(&duda_event_signals_list);
 
